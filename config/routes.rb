7bc5841dba58041ee90bab22344e9bacf091ff78
sumio96k <106654738+sumio96k@users.noreply.github.com>
2022-06-05 03:03:23 +0000

    [update] 削除機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   get 'books' => 'books#index'
   get 'books/:id' => 'books#show', as: 'book'
   get 'books/:id/edit' => 'books#edit', as: 'edit_list'
-
+  patch 'books/:id' => 'books#update', as: 'update_list'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_list'
   get 'top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
