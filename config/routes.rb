6ebd548076e5e0764e1a8b75727ce9f6f382aa40
sumio96k <106654738+sumio96k@users.noreply.github.com>
2022-06-05 08:45:22 +0000

    [update] トップ画面をルートパスに変更

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   get 'books/:id/edit' => 'books#edit', as: 'edit_list'
   patch 'books/:id' => 'books#update', as: 'update_list'
   delete 'books/:id' => 'books#destroy', as: 'destroy_list'
-  get 'top' => 'homes#top'
+  get '/' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
