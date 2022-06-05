a8b8ae78da6b3e5fdbdaae9d947296fa34ffb868
sumio96k <106654738+sumio96k@users.noreply.github.com>
2022-06-05 08:57:53 +0000

    [update] トップ画面をルートパスで表示

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'books/:id/edit' => 'books#edit', as: 'edit_list'
   patch 'books/:id' => 'books#update', as: 'update_list'
   delete 'books/:id' => 'books#destroy', as: 'destroy_list'
-  get '/' => 'homes#top'
+  
+  root to: 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
