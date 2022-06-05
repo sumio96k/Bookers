62fdb4304a95037c4fa963d60b7a7d503fbd99c4
sumio96k <106654738+sumio96k@users.noreply.github.com>
2022-06-05 15:25:40 +0000

    [update] サクセスメッセージの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,8 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-      redirect_to book_path(book.id)
+      flash[:notice] = "successfully"
+      redirect_to book_path(@book.id)
     else
       @books = Book.all
       render :index
@@ -24,9 +25,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "successfully"
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
